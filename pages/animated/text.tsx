d9303fc0f21f512ee24eb491857bf078267600b1
flukejaja <rockliverow90@hotmail.com>
2023-04-30 11:02:18 +0700

    4/30/2023 add loop animated

--- pages/animated/text.tsx
+++ pages/animated/text.tsx
@@ -1,54 +1,64 @@
-import { motion, } from "framer-motion";
-
+import { motion, AnimatePresence  } from "framer-motion";
+import { useState, useEffect } from "react";
 const AnimatedTextWord = ({ text }: any) => {
-    const words = text ? text.split(" ") : [''] as [String];
-    const container = {
-      hidden: { opacity: 0 },
-      visible: (i = 1) => ({
-        opacity: 1,
-        transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
-      }),
-    };
-  
-    const child = {
-      visible: {
-        opacity: 1,
-        x: 0,
-        transition: {
-          type: "spring",
-          damping: 12,
-          stiffness: 100,
-        },
-      },
-      hidden: {
-        opacity: 0,
-        x: 20,
-        transition: {
-          type: "spring",
-          damping: 12,
-          stiffness: 100,
-        },
-      },
-    };
-  
-    return (
-      <motion.div
-        style={{ overflow: "hidden", display: "flex", fontSize: "2.5rem" }}
-        variants={container}
-        initial="hidden"
-        animate="visible"
-      >
-        {words.map((word : string, index : number) => (
-          <motion.span
-            variants={child}
-            style={{ marginRight: "5px" }}
-            key={index}
-          >
-            {word}
-          </motion.span>
-        ))}
-      </motion.div>
-    );
+  const words = text ? text.split(" ") : [''] as [String];
+  const [isVisible, setIsVisible] = useState(true);
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
+    }),
   };
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsVisible(!isVisible);
+    }, 2000);
+
+    return () => clearTimeout(timeoutId);
+  }, [isVisible]);
+  const child = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 20,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
+
+  return (
+    <AnimatePresence>
+    {isVisible && <motion.div
+      style={{ overflow: "hidden", display: "flex", fontSize: "2.5rem" }}
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {words.map((word: string, index: number) => (
+        <motion.span
+          variants={child}
+          style={{ marginRight: "5px" }}
+          key={index}
+        >
+          {word}
+        </motion.span>
+      )
+      )}
+    </motion.div>}
+    </AnimatePresence>
+  );
+};
 
 export default AnimatedTextWord
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,14 +15,10 @@ import { motion, Variants } from "framer-motion";
 import AnimatedTextWord from './animated/text';
 import Head from 'next/head'
 
-export default function Home(props: { header: string }) {
+export default function Home() {
   gsap.registerPlugin(ScrollTrigger, MotionPathPlugin,);
   gsap.defaults({ ease: 'none' });
-  const svgRef = useRef(null);
-  const svgRefdiv4 = useRef(null);
-  const svgRefdiv1 = useRef(null);
   const ref = useRef(null);
-
   const slideDiv: Variants = {
     offscreen: {
       x: 60,
@@ -66,8 +62,6 @@ export default function Home(props: { header: string }) {
       }
     }
   };
-
-
   let settings = {
     dots: false,
     infinite: true,
@@ -78,8 +72,8 @@ export default function Home(props: { header: string }) {
     centerMode: true,
     arrows: false,
     speed: 3000,
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
+    autoplaySpeed: 0,
+    pauseOnHover: false,
     cssEase: "linear",
     draggable: false,
     responsive: [
@@ -111,14 +105,14 @@ export default function Home(props: { header: string }) {
   let settingsProfile = {
     infinite: true,
     slidesToShow: 1,
-    speed: 3000,
+    speed: 10000,
     cssEase: "linear",
     arrows: false,
     dots: true,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: true
+    autoplaySpeed: 0,
+    pauseOnHover: false
   };
   useEffect(() => {
     const element = ref.current as any
@@ -144,66 +138,6 @@ export default function Home(props: { header: string }) {
       }
     );
   }, []);
-
-  // useEffect(() => {
-  //   const svg = svgRef.current;
-  //   const svg1 = svgRefdiv1.current;
-  //   const svg4 = svgRefdiv4.current;
-  //   if (svg) {
-  //     gsap.timeline({
-  //       defaults: { duration: 1 },
-  //       scrollTrigger: {
-  //         trigger: svg,
-  //         scrub: true,
-  //         start: 'top center',
-  //         end: 'bottom center',
-  //       },
-  //     }).from('.theLine', { drawSVG: 1 }, 0)
-  //   }
-  //   if (svg4) {
-  //     gsap.timeline({
-  //       defaults: { duration: 1 },
-  //       scrollTrigger: {
-  //         trigger: svg4,
-  //         scrub: true,
-  //         start: 'top center',
-  //         end: 'bottom center',
-  //       },
-  //     }).from('.theLine1', { drawSVG: 1 }, 0)
-  //   }
-  //   if (svg1) {
-  //     gsap.timeline({
-  //       defaults: { duration: 1 },
-  //       scrollTrigger: {
-  //         trigger: svg1,
-  //         scrub: true,
-  //         start: 'top center',
-  //         end: 'bottom center',
-  //       },
-  //     }).from('.theLine2', { drawSVG: 1 }, 0)
-  //   }
-  // }, []);
-
-  // const titleRef = useRef(null);
-
-  // useEffect(() => {
-  //   const tl = gsap.timeline({ repeat: -1, yoyo: true });
-  //   tl.to(document.querySelectorAll('.title .word'), {
-  //     opacity: 1,
-  //     y: '0%',
-  //     rotation: '0deg',
-  //     ease: 'power4.inOut',
-  //     duration: 1.2,
-  //     stagger: 0.5,
-  //   });
-  //   gsap.set(document.querySelectorAll('.title'), { opacity: 1 });
-  //   gsap.set(document.querySelectorAll('.title .word'), { opacity: 0.0001, y: '200%', rotation: '3deg' });
-  //   new SplitText(document.querySelector('.title'), {
-  //     type: 'lines words',
-  //     linesClass: 'line line--++',
-  //     wordsClass: 'word word--++'
-  //   });
-  // }, []);
   const image = ['/image/next-js-icon-512x512-zuauazrk.png', '/image/Fastify.png', '/image/socket-io-logo-png-transparent.png', '/image/Tailwind_CSS_Logo.svg.png'
     , '/image/4195cf989fac0128a89669f40a1e3496.png', '/image/5847f40ecef1014c0b5e488a.png', '/image/django-logo-negative.png', '/image/prismaHD.png', '/image/communityIcon_4g1uo0kd87c61.png', '/image/Vue.png']
 
@@ -221,10 +155,9 @@ export default function Home(props: { header: string }) {
       >
         <div id='Home' className='h-[50rem] flex flex-wrap justify-center items-center text-black first'>
           <div className='flex flex-col  md:flex-row h-full w-full justify-center items-center relative'>
-            <div className='w-full flex flex-col items-center justify-center  h-full '>
-              <div className='h-44'></div>
+            <div className='w-full flex flex-col items-center justify-center h-1/2  md:h-full '>
+              <div className='h-52'></div>
               <div className='flex flex-col  h-full w-1/2 items-center space-y-5 ' >
-                {/* <p className='text-2xl md:text-6xl '>Hello</p> */}
                 <AnimatedTextWord text="Hi" />
                 <AnimatedTextWord text="I'm Pakkapong Sittiporn" />
                 <AnimatedTextWord text="And" />
@@ -232,8 +165,8 @@ export default function Home(props: { header: string }) {
               </div>
 
             </div>
-            <div className='w-full h-full flex justify-center items-center relative overflow-hidden pt-48 md:pt-0'>
-              <div className='h-[40rem] w-[40rem] max-w-full md:animate-movemask bg-black' style={{
+            <div className='w-full h-1/2  md:h-full flex justify-center items-center relative overflow-hidden md:pt-0'>
+              <div className='h-[20rem] w-[20rem] md:h-[40rem] md:w-[40rem] max-w-full md:animate-movemask bg-black' style={{
                 WebkitMask: "url('/image/Regular_blob.svg') no-repeat",
                 mask: "url('/image/Regular_blob.svg') no-repeat"
 
@@ -241,7 +174,7 @@ export default function Home(props: { header: string }) {
                 <Image
                   src='/image/53e98f5701d854bc1f9295cc7fbb25f6.jpg'
                   alt=''
-                  className='object-cover'
+                  className='object-cover w-full h-full'
                   fill
                 />
               </div>
@@ -295,9 +228,6 @@ export default function Home(props: { header: string }) {
               </div>
             </div>
           </div>
-          {/* <svg ref={svgRefdiv1} height="100%" className='absolute inset-y-0 right-0 w-24 md:w-48'>
-            <path className='theLine2' d="M 50 0 L 50 800" stroke="#E30B5C" strokeWidth="5" fill="none" />
-          </svg> */}
           <div className='absolute -bottom-10 right-0 h-16 w-full'>
             <motion.svg
               width="100%"
@@ -407,22 +337,15 @@ export default function Home(props: { header: string }) {
                 </div>
               </div>
             </motion.div>
-            {/* <svg ref={svgRef} height="100%" className='absolute inset-y-0 right-0 w-24 md:w-48'>
-              <path className='theLine' d="M 50 0 L 50 640" stroke='white' strokeWidth="5" fill="none" />
-              <path className='theLine' d="M 50 0 L 50 320 C 50 440, 150 440, 150 520 C 150 600, 50 600, 50 640" stroke='#E30B5C' strokeWidth="5" fill="none" />
-            </svg>
-            <svg ref={svgRef} height="100%" className='absolute inset-y-0 left-0 w-[40%]'>
-              <path className='theLine' d="M 0 50 H 640" stroke='white' strokeWidth="5" fill="none" />
-            </svg> */}
           </motion.div>
         </div>
         <div id='Contact' className='h-[40rem] bg-black flex justify-center items-center relative text-black px-10 py-10'>
-          <motion.div 
+          <motion.div
             initial="offscreen"
             whileInView="onscreen"
             viewport={{ once: true, amount: 0.7 }}
             className='bg-white w-[40rem] max-w-full px-10 flex md:flex-row flex-col h-[30rem] items-center rounded-3xl shadow-2xl shadow-pink-500  justify-center'>
-            <motion.div variants={slideDiv} className='w-full  h-full flex justify-center items-center'>
+            <motion.div variants={slideDiv} className='w-full  h-full flex justify-center items-center md:flex-row flex-col'>
               <div >
                 <div className='h-44 w-44 rounded-full overflow-hidden relative'>
                   <Image src={'/image/53e98f5701d854bc1f9295cc7fbb25f6.jpg'} alt='proflie' fill className='h-full w-full object-fit' />
@@ -434,10 +357,6 @@ export default function Home(props: { header: string }) {
               </div>
             </motion.div>
           </motion.div>
-
-          {/* <svg ref={svgRefdiv4} height="100%" className='absolute inset-y-0 right-0 w-24 md:w-48'>
-            <path className='theLine1 md:bg-white bg-black' d="M 50 0 L 50 640" stroke='#E30B5C' strokeWidth="5" fill="none" />
-          </svg> */}
         </div>
       </ScrollSpy>
     </div>
